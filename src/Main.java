f9d6ff72d5a65936879a351ffdd7b991b6974c91
Mishator <voyteh89@mail.ru>
2023-03-06 21:27:09 +0300

    Выполнена задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
     }
+    public static void task1 () {
+        System.out.println("Задача 1");
+        int clientOs = 1;
+        if (clientOs == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+    }
+
 }
