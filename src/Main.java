4635857724fc9fb6641ee87051184f1f44f5197d
Mishator <voyteh89@mail.ru>
2023-03-07 20:58:24 +0300

    Выполнена задача 5

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         task2();
         task3();
         task4();
+        task5();
     }
     public static void task1 () {
         System.out.println("Задача 1");
@@ -52,6 +53,34 @@ public class Main {
                 deliveryDays++;
             }
             System.out.println("Потребуется дней: " + deliveryDays);
+        }
+    }public static void task5 () {
+        System.out.println("Задача 5");
+        int monthNumber = 13;
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Зима!");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна!");
+                break;
+                case 6:
+                case 7:
+                case 8:
+                    System.out.println("Лето!");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    System.out.println("Осень!");
+                    break;
+            default:
+                System.out.println("Укажите правильный месяц!");
+
         }
     }
 
